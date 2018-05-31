4bbf1f91c773792944d02ee71376cf0b38c99668
David Ngo and Eliran Maimon <eliran@>
2018-05-31 10:59:25 -0700

    Create beginnings of an address book

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,32 @@ $(function() {
     var inputAdd2 = $("input#inputAdd2").val();
     var inputCity = $("input#inputCity").val();
     var inputZip = $("input#inputZip").val();
-    console.log(inputAdd1);
-    console.log(inputAdd2);
-    console.log(inputZip);
     if(inputAdd2 == "") {
       $("#receiptBody").html("Thank you for your purchase " + inputName + ".</br>Shipping confirmed to following address:</br>" + inputAdd1 + "</br>" + inputCity + ", " + inputZip);
     } else {
       $("#receiptBody").html("Thank you for your purchase " + inputName + ".</br>Shipping confirmed to following address:</br>" + inputAdd1 + "</br>" + inputAdd2 + "</br>" + inputCity + ", " + inputZip);
     };
   });
+
+  $("form#addressBook").submit(function(event) {
+    var contactName = $("input#contactName").val();
+    var contactAdd = $("input#contactAdd").val();
+    var contactPhone = $("input#contactPhone").val();
+    var contactEmail = $("input#contactEmail").val();
+    $("#contacts").append("<li id=\"clickName\">" + contactName + "</li>");
+    $("#contacts").append("<li class=\"info\">" + contactAdd + "</li>");
+    $("#contacts").append("<li class=\"info\">" + contactPhone + "</li>");
+    $("#contacts").append("<li class=\"info\">" + contactEmail + "</li>");
+    event.preventDefault();
+
+    $("#clickName").click(function() {
+      console.log("reached");
+      $(".info").toggle();
+
+    });
+  });
+
+
+
 
 });
